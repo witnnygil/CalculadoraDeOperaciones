78c246ed2a54f38672a06796c1d5bc049b6a520d
witnnygil <anayahgil@gmail.com>
2026-07-17 00:12:15 -0400

    Agrega validacion para realizar las operaciones

--- CalculadoraDeOperaciones/FrmCalculatorOperations.cs
+++ CalculadoraDeOperaciones/FrmCalculatorOperations.cs
@@ -10,26 +10,39 @@ namespace CalculadoraDeOperaciones
 
         private void BtnAdd_Click(object sender, EventArgs e)
         {
-            var result = Calculator.Add(double.Parse(TxtFirstNumber.Text), double.Parse(TxtSecondNumber.Text));
-            TxtResult.Text = result.ToString();
+            if (TxtFirstNumber.Text != string.Empty && TxtSecondNumber.Text != string.Empty)
+            {
+                var result = Calculator.Add(double.Parse(TxtFirstNumber.Text), double.Parse(TxtSecondNumber.Text));
+                TxtResult.Text = result.ToString();
+            }
+            
         }
 
         private void BtnSubstract_Click(object sender, EventArgs e)
         {
-            var result = Calculator.Subtract(double.Parse(TxtFirstNumber.Text), double.Parse(TxtSecondNumber.Text));
-            TxtResult.Text = result.ToString();
+            if (TxtFirstNumber.Text != string.Empty && TxtSecondNumber.Text != string.Empty)
+            {
+                var result = Calculator.Subtract(double.Parse(TxtFirstNumber.Text), double.Parse(TxtSecondNumber.Text));
+                TxtResult.Text = result.ToString();
+            }
         }
 
         private void BtnMultiply_Click(object sender, EventArgs e)
         {
-            var result = Calculator.Multiply(double.Parse(TxtFirstNumber.Text), double.Parse(TxtSecondNumber.Text));
-            TxtResult.Text = result.ToString();
+            if (TxtFirstNumber.Text != string.Empty && TxtSecondNumber.Text != string.Empty)
+            {
+                var result = Calculator.Multiply(double.Parse(TxtFirstNumber.Text), double.Parse(TxtSecondNumber.Text));
+                TxtResult.Text = result.ToString();
+            }
         }
 
         private void BtnDivide_Click(object sender, EventArgs e)
         {
-            var result = Calculator.Divide(double.Parse(TxtFirstNumber.Text), double.Parse(TxtSecondNumber.Text));
-            TxtResult.Text = result.ToString();
+            if (TxtFirstNumber.Text != string.Empty && TxtSecondNumber.Text != string.Empty)
+            {
+                var result = Calculator.Divide(double.Parse(TxtFirstNumber.Text), double.Parse(TxtSecondNumber.Text));
+                TxtResult.Text = result.ToString();
+            }
         }
 
         private void BtnClean_Click(object sender, EventArgs e)
